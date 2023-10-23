c4911fd1832ab2abe60116932eea9acfaea427b8
parthwohlig <parthwohlig@gmail.com>
2023-10-23 16:53:33 +0530

    first

--- services/userActivityLogs/userActivityLogsService.js
+++ services/userActivityLogs/userActivityLogsService.js
@@ -1,17 +1,17 @@
-// const AuditLogs = require('../../mongooseSchema/AuditLogs')
+const AuditLogs = require('../../mongooseSchema/AuditLogs')
 const client = require('./../../lib/db/elastic')
 const __constants = require('./../../config/constants')
 
 class UserActivityLogs {
-  // userActivitySaveToMongo (body) {
-  //   try {
-  //     const auditlogs = new AuditLogs(body)
-  //     auditlogs.save() // when fail its goes to catch
-  //   } catch (err) {
-  //     console.log('Error in userActivitySave function while inserting into mongo database :: ', err)
-  //     throw new Error(err)
-  //   }
-  // }
+  userActivitySaveToMongo (body) {
+    try {
+      const auditlogs = new AuditLogs(body)
+      auditlogs.save() // when fail its goes to catch
+    } catch (err) {
+      console.log('Error in userActivitySave function while inserting into mongo database :: ', err)
+      throw new Error(err)
+    }
+  }
 
   async userActivitySaveToElastic (body) {
     try {
